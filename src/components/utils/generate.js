1d09eda21ed472c1b7e724d7a8c62abff1a299df
mandip1213 <thapamandip1213@gmail.com>
2021-09-02 00:35:47 +0545

    heading color changed

--- src/components/utils/generate.js
+++ src/components/utils/generate.js
@@ -6,7 +6,7 @@ const checkGcd = (e, phiFunction) => {
     }
 }
 export const isPrime = (num) => {
-    for (let i = 2; i < num; i++) {
+    for (let i = 2; i < Math.sqrt(num); i++) {
         if (num % i === 0) return false;
     }
     return true;
